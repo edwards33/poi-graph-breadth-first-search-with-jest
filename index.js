98e8abde74ad36fd03905029cba7bb3da44e51af
Edward <edylennyy@gmail.com>
2019-05-06 00:46:19 +0300

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+var Dracula = require('graphdracula')
+
+const { createGraph, createNode } = require('./graph')
+
+const graph = createGraph(true)
+
+const nodes = ['a', 'b', 'c', 'd', 'e', 'f']
+const edges = [
+  ['a', 'b'],
+  ['a', 'e'],
+  ['a', 'f'],
+  ['b', 'd'],
+  ['b', 'e'],
+  ['c', 'b'],
+  ['d', 'c'],
+  ['d', 'e']
+]
+
+nodes.forEach(node => {
+  graph.addNode(node)
+})
+
+edges.forEach(nodes => {
+  graph.addEdge(...nodes)
+})
+
+graph.bfs('a', node => {
+  console.log(node.key)
+})
+
+document.getElementById("app").innerHTML = `
+      <h1>Data Structures in JS using POI and Jest</h1>
+      <h2>Breadth First Search Example</h2>
+      <p> Open developer console </p>
+    `
+
